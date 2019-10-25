1d370675d3ebaab7b18937b39f3e6889d3138159
Joseph-Emmanuel Banzio <banzio@et.esiea.fr>
2019-10-25 03:14:11 +0200

    adding people from a txt file

--- lbot.py
+++ lbot.py
@@ -16,7 +16,7 @@ def log_into_linkedin(driver):
     username = driver.find_element_by_id('username')
     username.send_keys("junior-esiea@et.esiea.fr")
     password = driver.find_element_by_id('password')
-    password.send_keys(unicode("passwordstillfakebecauseofsecurity", 'utf-8'))
+    password.send_keys(unicode("passwordstillfakebecauseofsecurity", 'utf-8')) #
     password.send_keys(Keys.RETURN)
     print('connection successful')
 
@@ -25,6 +25,7 @@ def searchSomeone(driver, name):
     search = driver.find_element_by_class_name('search-global-typeahead__input')
     search.send_keys(name)
     search.send_keys(Keys.RETURN)
+    search.send_keys("tamere")
     print('search step')
     
 def addAlumni(driver):
@@ -32,11 +33,9 @@ def addAlumni(driver):
         sendInvit_button = driver.find_element_by_xpath('//button[@class="search-result__action-button search-result__actions--primary artdeco-button artdeco-button--default artdeco-button--2 artdeco-button--secondary"]')
         sendInvit_button.click()
         time.sleep(2)
-        #endInvit_button = driver.find_element_by_xpath('//button[@class="ml1 artdeco-button artdeco-button--3 artdeco-button--primary ember-view"]')
-        #endInvit_button.click()
+
         print("invitation sent")
         
-        time.sleep(2)
         addNote_button = driver.find_element_by_xpath('//button[@aria-label="Ajouter une note"]')
         addNote_button.click()
         message = "Junior ESIEA bot used to create the Alumni group. We sent you a friend request because you were a member of Junior ESIEA or PIER, the small entreprise of the engineering school ESIEA."
@@ -51,18 +50,23 @@ def addAlumni(driver):
     except:
         print("request already sent or alumni already in your network")
    
-
+def readAlumns(filename, driver):
+    alumnis = open(filename, 'r')
+    for alumni in alumnis:
+        searchSomeone(driver, unicode(alumni,'utf-8'))
+        time.sleep(5)
+        addAlumni(driver)
+        driver.get('https://www.linkedin.com/mynetwork/')
     
 def main():
     url = "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin"
-    browser = webdriver.Firefox()
-    browser.get(url)
+    driver = webdriver.Firefox()
+    driver.get(url)
     #goto_signinpage(browser)
-    log_into_linkedin(browser)
+    log_into_linkedin(driver)
     time.sleep(5)
-    searchSomeone(browser, unicode('Joseph-Emmanuel Banzio','utf-8'))
-    time.sleep(5)
-    addAlumni(browser)
+    readAlumns("alumnis.txt", driver)
+    
     
     
     
